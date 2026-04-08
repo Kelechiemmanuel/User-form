6ace3bfae4cac960074e12151b7a9835cf9ca417
kelechiemmanuel <ekelechi413@gmail.com>
2026-04-08 16:39:17 +0100

    done

--- form/src/component/Demo3.jsx
+++ form/src/component/Demo3.jsx
@@ -23,7 +23,7 @@ export const Demo3 = () => {
                         <input type="password" placeholder='password' className='border-2 border-gray-400 rounded-md py-2 px-3 outline-none w-full' />
                     </div>
                     <div className='w-full relative'>
-                    <button className='w-full border-2 border-gray-400 rounded-md py-2'>
+                    <button className='w-full border-2 border-gray-400 rounded-md py-2 cursor-pointer'>
                         {isSignIn ? 'Sign Up' : 'Login'}
                     </button>
                     </div>
@@ -37,7 +37,7 @@ export const Demo3 = () => {
                         {isSignIn ? 'Already have an account?' : "Don't have an account"}
                     </p>
                     <div>
-                        <button onClick={() => setIsSignIn(!isSignIn)} className='border border-white px-6 py-2 rounded-md'>
+                        <button onClick={() => setIsSignIn(!isSignIn)} className='border border-white px-6 py-2 rounded-md cursor-pointer'>
                             {isSignIn ? 'Login' : 'Sign Up'}
                         </button>
                     </div>
